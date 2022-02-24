2e009c9fdd8ddc94ca717392d70fdc9448642b1d
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-24 10:52:47 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-tar -xvf nanominer-linux-3.5.2.tar.gz
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2-cuda11.tar.gz
+tar -xvf nanominer-linux-3.5.2-cuda11.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf config_verus.ini
 wget -O config_verus.ini https://raw.githubusercontent.com/nurfasikin/verust/main/config_verus.ini

97cfac27da9b48dc624f62fa7894a87e234012e4
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-19 00:27:35 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,4 @@
 wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-chmod 700 nanominer
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf config_verus.ini
